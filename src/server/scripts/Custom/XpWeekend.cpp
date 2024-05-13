dedcbffc762cd8cf5f4aa5c3de39b227439528d0
11011010 <rar.rar@abwesend.de>
2024-05-14 01:27:46 +0300

    forgot a few races.

--- src/server/scripts/Custom/XpWeekend.cpp
+++ src/server/scripts/Custom/XpWeekend.cpp
@@ -13,14 +13,19 @@
         {    
 
 
-    float reductionPercentage = 0.2f; // 20% reduction
+    float reductionPercentage = 0.2f; // 20% reduction for the following allied races.
     
     if (player->getRace() == RACE_NIGHTBORNE || 
         player->getRace() == RACE_HIGHMOUNTAIN_TAUREN || 
         player->getRace() == RACE_VOID_ELF || 
-        player->getRace() == RACE_LIGHTFORGED_DRAENEI || 
-        player->getRace() == RACE_DARK_IRON_DWARF || 
-        player->getRace() == RACE_MAGHAR_ORC)
+        player->getRace() == RACE_LIGHTFORGED_DRAENEI ||
+		player->getRace() == RACE_ZANDALARI_TROLL  || 
+		player->getRace() == RACE_KUL_TIRAN  ||		
+   		player->getRace() == RACE_DARK_IRON_DWARF ||
+		player->getRace() == RACE_VULPERA ||
+		player->getRace() == RACE_MECHAGNOME ||  
+  		player->getRace() == RACE_MAGHAR_ORC)
+		
     {
         float reducedAmount = amount * (1 - reductionPercentage);        
         amount = static_cast<uint32>(reducedAmount);    
