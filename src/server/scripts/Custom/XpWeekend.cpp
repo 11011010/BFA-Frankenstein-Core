139b14a26f6d39125003a31528532ee4d5706e7a
11011010 <rar.rar@abwesend.de>
2024-05-14 01:19:41 +0300

    this should be much better

--- src/server/scripts/Custom/XpWeekend.cpp
+++ src/server/scripts/Custom/XpWeekend.cpp
@@ -10,20 +10,30 @@
     public:
         XpWeekend() : PlayerScript("XpWeekend") { }
         void OnGiveXP(Player* player, uint32& amount, Unit* victim)override
-        {
-			uint32 reduction = 0.8f;
-		   	if (player->getRace() == RACE_NIGHTBORNE)
-                amount = amount * reduction;
-             if (player->getRace() == RACE_HIGHMOUNTAIN_TAUREN)
-                 amount = amount * reduction;
-             if (player->getRace() == RACE_VOID_ELF)
-                 amount = amount * reduction;
-             if (player->getRace() == RACE_LIGHTFORGED_DRAENEI)
-                 amount = amount * reduction;;
-             if (player->getRace() == RACE_DARK_IRON_DWARF)
-                 amount = amount * reduction;
-             if (player->getRace() == RACE_MAGHAR_ORC)
-                 amount = amount * reduction;
+        {    
+
+
+    float reductionPercentage = 0.2f; // 20% reduction
+    
+    if (player->getRace() == RACE_NIGHTBORNE || 
+        player->getRace() == RACE_HIGHMOUNTAIN_TAUREN || 
+        player->getRace() == RACE_VOID_ELF || 
+        player->getRace() == RACE_LIGHTFORGED_DRAENEI || 
+        player->getRace() == RACE_DARK_IRON_DWARF || 
+        player->getRace() == RACE_MAGHAR_ORC)
+    {
+        float reducedAmount = amount * (1 - reductionPercentage);
+        
+        // Make sure the new amount is not less than 0
+        if (reducedAmount < 0) reducedAmount = 0;
+        
+        amount = static_cast<uint32>(reducedAmount);    
+    }
+
+
+    
+    // No reduction for other races
+
 			
             if (sConfigMgr->GetBoolDefault("DynamicXP.Enable", true))
             {
