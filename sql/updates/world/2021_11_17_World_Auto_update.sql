662c7d46460053cb1d13e6562031ee27046cfb82
11011010 <69040574+11011010@users.noreply.github.com>
2024-06-17 11:55:34 +0300

    Updated main DB to latest, including fixes.

--- sql/updates/world/2021_11_17_World_Auto_update.sql
+++
@@ -1,7 +0,0 @@
-/*
-**************************
-*    BfaCore Reforged    *
-**************************
-*/
-
-UPDATE `world_test`.`updates_include` SET `path`='$/sql/updates/world' WHERE  `path`='$/sql/LatincoreBfa/world';
